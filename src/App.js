59b4447f99d02543a71863481b03988332da63a1
vivek <vivekrsq@gmail.com>
2022-02-03 15:29:20 +0530

    finally form submitted to mongo atlas

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Form from './components/Form';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <>
+    <Navbar />
+    <Form />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Form.js
@@ -0,0 +1,67 @@
+import React, { useState } from 'react';
+import logo from './profile-logo.jpg';
+
+function Form() {
+    const [values, setValues] = useState({
+        name: "",
+        class: "",
+        email: ""
+    });
+    
+    let handleChange = (e)=> {
+        setValues(()=>{
+            return{
+                ...values,
+                [e.target.id]: e.target.value,
+            }
+        } 
+        );
+    }
+    
+    let onSubmit = (e)=>{
+        e.preventDefault();
+        console.log(values);
+        fetch('http://localhost:3001/create', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            
+            body: JSON.stringify(values)
+          }).then(res => res.json()).then(data => console.log(data));
+          
+        
+        // const response = postData();
+        // console.log(response);
+    }
+
+
+    return (
+        <div className="container w-25 p-3 my-4 border border-primary">
+            <img src={logo} className="rounded mx-auto d-block w-25" alt="profile-image"></img>
+            <form>
+
+                <div className="mb-3">
+                    <label htmlFor="name" className="form-label">Name</label>
+                    <input type="text" className="form-control border border-primary" value={values.password} onChange={handleChange} id="name"/>
+                </div>
+
+                <div className="mb-3">
+                    <label htmlFor="class" className="form-label">Class</label>
+                    <input type="text" className="form-control border border-primary" value={values.password} onChange={handleChange} id="class"/>
+                </div>
+
+                <div className="mb-3">
+                    <label htmlFor="email" className="form-label">Email address</label>
+                    <input type="email" className="form-control border border-primary" value={values.email} onChange={handleChange} id="email" aria-describedby="emailHelp"/>
+                        
+                </div>
+                
+                
+                <button type="submit" className="btn btn-primary w-100" onClick={onSubmit}>Submit</button>
+            </form>
+        </div>
+    );
+}
+
+export default Form;
